24c5634628309d80791a95cb6332cf2c12927180
srdurham <stephen.durham@floridadep.gov>
2024-01-23 18:17:44 -0600

    Realized the updated threshold/quantile zeroes need to be negative so that legit zeroes are not flagged.

--- IndicatorQuantiles.R
+++ IndicatorQuantiles.R
@@ -1083,10 +1083,10 @@ qs2 <- merge(qs2, refdat[ThresholdID %in% unique(qs2$ThresholdID), .(ThresholdID
 qs2 <- rbind(qs2, refdat[ThresholdID %in% setdiff(refdat$ThresholdID, qs2$ThresholdID), ])
 setkey(qs2, NULL) #remove any key created when merging files by ThresholdID
 
-#Update any threshold or quantile values that equal zero to 0.000001 because flagging in the DDI is based on </>, not <=/>=
-refdat[LowThreshold == 0, LowThreshold := 0.000001]
-qs2[LowThreshold == 0, LowThreshold := 0.000001]
-qs2[LowQuantile == 0, LowQuantile := 0.000001]
+#Update any threshold or quantile values that equal zero to -0.000001 so that legitimate zeros aren't flagged (flagging in the DDI is based on </>, not <=/>=)
+refdat[LowThreshold == 0, LowThreshold := -0.000001]
+qs2[LowThreshold == 0, LowThreshold := -0.000001]
+qs2[LowQuantile == 0, LowQuantile := -0.000001]
 
 #Update column and row ordering
 setcolorder(qs2, c("ThresholdID", "ParameterID", "Habitat", "IndicatorID", "IndicatorName", "CombinedTable", "ParameterName", "Units", "LowThreshold", "HighThreshold", "QuadSize_m2", 
